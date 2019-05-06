ab037c8b0a5b7231742d304794e767c462020c54
crook007 <anuj.bhelkar@gmail.com>
2019-05-06 19:34:24 +0530

    Add Testing program

--- OOP/StockAccount.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs');
-let utility = require('./StockAccountUtil');
-let data = fs.readFileSync("/home/admin1/fellowShipPrograms/OOP/JSONFiles/StockAccount.json");
-let JSONData = JSON.parse(data);
-function stock(){
-    utility.stockAccount(JSONData);
-}
-stock();
